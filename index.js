edab5090b4ec7437b5a8eec79c3e8566856c8021
boxiercarpet <boxiercarpet@gmail.com>
2024-02-27 23:56:31 +0100

    feat: Add old info command

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ const commands = [
                 .setRequired(true)
         )
         .setDefaultMemberPermissions("0"),
+    new SlashCommandBuilder()
+        .setName("info2")
+        .setDescription("Advance info about Fivem player")
+        .addStringOption((option) =>
+            option
+                .setName("query")
+                .setDescription("Player name/discord/steam")
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions("0"),
 ];
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
@@ -45,6 +55,31 @@ client.on("ready", () => {
 
 async function getPlayerInfo(query) {
     const searchUrl =
+        "https://api.chub.pl/fivem/players/search?" +
+        new URLSearchParams({ query: query });
+
+    const res = await fetch(searchUrl, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${process.env.TOKEN}` },
+    });
+
+    const players = await res.json();
+
+    if (!players || players.length <= 0) return null;
+
+    const res2 = await fetch(
+        `https://api.chub.pl/fivem/players/${players[0].id}/servers`,
+        {
+            method: "GET",
+            headers: { Authorization: `Bearer ${process.env.TOKEN}` },
+        }
+    );
+
+    return { ...players[0], servers: await res2.json() };
+}
+
+async function getPlayerAdvanceInfo(query) {
+    const searchUrl =
         "https://api.chub.pl/fivem/players/advance?" +
         new URLSearchParams({ query: query });
 
@@ -63,6 +98,53 @@ client.on("interactionCreate", async (interaction) => {
 
     if (interaction.commandName === "info") {
         const info = await getPlayerInfo(
+            interaction.options.getString("query")
+        );
+
+        if (!info) return interaction.reply("Nie znaleziono gracza.");
+
+        info.servers.sort(
+            (a, b) => new Date(b.last_seen) - new Date(a.last_seen)
+        );
+
+        const formattedServers = info.servers.map((server) => {
+            const date = new Date(server.last_seen);
+            const fd = date.toLocaleString("pl-PL", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit",
+            });
+
+            return {
+                name: `Nazwa serwera: ${server.hostname.replaceAll(
+                    /\^\d/g,
+                    ""
+                )}`,
+                value: `Ostatni connect: ${fd}`,
+            };
+        });
+
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle(`[${info.id}] ${info.name}`)
+                    .addFields([
+                        {
+                            name: "Identifiers",
+                            value: `${info.discord}\n${info.steam}\n${info.license}`,
+                        },
+                        ...formattedServers,
+                    ])
+                    .setFooter({ text: "©️ CreativeHub hosted by Trujca.gg" })
+                    .setTimestamp(),
+            ],
+        });
+    }
+
+    if (interaction.commandName === "info2") {
+        const info = await getPlayerAdvanceInfo(
             interaction.options.getString("query")
         );
 
